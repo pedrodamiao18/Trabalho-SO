85bdb16a5455ac0cb04ec1411b0060bd8285ed2c
Pedro Cardoso <pedrodamiao18@gmail.com>
2025-04-24 14:19:18 +0100

    correções

--- scheduler.c
+++ scheduler.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "prob.h"
 
-void sort_by_arrival(Process process[], int n) {
+void sort_by_burst(Process process[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (process[j].arrival_time > process[j + 1].arrival_time) {
+            if (process[j].burst_time < process[j + 1].burst_time) {
                 Process temp = process[j];
                 process[j] = process[j + 1];
                 process[j + 1] = temp;
@@ -16,35 +16,35 @@ void sort_by_arrival(Process process[], int n) {
 //First-Come, First-Served (FCFS)
 void schedule_fcfs(Process process[], int n)
 {
-    sort_by_arrival(process, n);
+    sort_by_burst(process, n);
 
     int time = 0;
     double total_waiting = 0;
     double total_turnaround = 0;
 
     printf("\n Resultados FCFS:\n");
-    printf("| PID | Arrival | Start | Finish | Waiting | Turnaround |\n");
+    printf("| PID | Burst   | Start | Finish | Waiting | Turnaround |\n");
     printf("|-----|---------|-------|--------|---------|------------|\n");
 
     for (int i = 0; i < n; i++) {
-        double arrival = process[i].arrival_time;
+        double burst = process[i].burst_time;
         
         int start;
-        if (time < arrival)
-            start = arrival;
+        if (time < burst)
+            start = burst;
         else
             start = time;
             
         int finish = start + process[i].burst_time;
-        int waiting = start - arrival;
-        int turnaround = finish - arrival;
+        int waiting = start - burst;
+        int turnaround = finish - burst;
 
         total_waiting += waiting;
         total_turnaround += turnaround;
         time = finish;
 
         printf("| %3d | %7.2f | %5d | %6d | %7d | %10d |\n",
-               process[i].id, arrival, start, finish, waiting, turnaround);
+               process[i].id, burst, start, finish, waiting, turnaround);
     }
 
     printf("\n Estatísticas:\n");
@@ -107,10 +107,11 @@ void schedule_sjf(Process process[], int n) {
 
 //Priority Scheduling (Preemptive and Non-Preemptive)
 void schedule_priority(Process process[], int n) {
-    int time = 0;
+    double time = 0;
     int completed = 0;
     int done[n];
-    for (int i = 0; i < n; i++) done[i] = 0;
+    for (int i = 0; i < n; i++) 
+        done[i] = 0;
 
     double total_waiting = 0;
     double total_turnaround = 0;
@@ -124,18 +125,30 @@ void schedule_priority(Process process[], int n) {
 
         for (int i = 0; i < n; i++) {
             if (!done[i] && process[i].arrival_time <= time) {
-                if (best == -1 || process[i].priority < process[best].priority)
+                if (
+                    best == -1 ||
+                    process[i].priority < process[best].priority || 
+                    (process[i].priority == process[best].priority &&
+                     process[i].burst_time < process[best].burst_time)
+                ) {
                     best = i;
+                }
             }
         }
 
         if (best == -1) {
-            time++; 
+            double min_arrival = 1000;
+            for (int i = 0; i < n; i++) {
+                if (!done[i] && process[i].arrival_time < min_arrival) {
+                    min_arrival = process[i].arrival_time;
+                }
+            }
+            time = min_arrival;
             continue;
         }
 
-        int start = time;
-        int finish = start + process[best].burst_time;
+        double start = time;
+        double finish = start + process[best].burst_time;
         int waiting = start - process[best].arrival_time;
         int turnaround = finish - process[best].arrival_time;
 
@@ -146,12 +159,12 @@ void schedule_priority(Process process[], int n) {
         done[best] = 1;
         completed++;
 
-        printf("| %3d | %8d | %7.2f | %5.2f | %5d | %6d | %7d | %10d |\n",
-               process[best].id,
-               process[best].priority,
-               process[best].arrival_time,
-               process[best].burst_time,
-               start, finish, waiting, turnaround);
+        printf("| %3d | %8d | %7.2f | %5.2f | %5.2f | %6.2f | %7d | %10d |\n",
+            process[best].id,
+            process[best].priority,
+            process[best].arrival_time,
+            process[best].burst_time,
+            start, finish, waiting, turnaround);
     }
 
     printf("\n Estatísticas:\n");
